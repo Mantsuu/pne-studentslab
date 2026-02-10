d69a395f4c6ae21e19199d4ea93b67a279f5d5c3
mantsuu <m.gurung.2025@alumnos.urjc.es>
2026-02-10 15:48:44 +0100

    I completed exercise 2 of S03

---
+++ S03/dna_count.py
@@ -0,0 +1,21 @@
+def count_DNA(letter):
+    count = 0
+    c_A = 0
+    c_C = 0
+    c_T = 0
+    c_G = 0
+    while count < len(letter):
+        if letter[count] == "A":
+            c_A += 1
+        elif letter[count] == "C":
+            c_C += 1
+        elif letter[count] == "T":
+            c_T += 1
+        elif letter[count] == "G":
+            c_G += 1
+        count += 1
+    return c_A, c_C, c_T, c_G
+DNA = input("Enter a DNA sequence: ")
+print("Total length: ", len(DNA))
+A, C, T, G = count_DNA(DNA)
+print(" A:", A, " C:", C, " T:", T, " G:", G)
